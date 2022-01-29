f6a8d4b906174964903eb9c21c11fa131e03c0b9
MOLDOGAZY <moldogazy.kabylbekov.kk@gmail.com>
2022-01-29 13:14:51 +0600

    fix task 5

--- 02w-basics/js/w02-hw5.js
+++ 02w-basics/js/w02-hw5.js
@@ -4,17 +4,8 @@
     Пример: "Номер вашей карты: **********1245"
  */
 
-const CARD_NUMBER = prompt("Введите номер карты", 1234567891011245);
-const CARD_NUMBER_LAST_FOUR = CARD_NUMBER.slice(-4);
-const CARD_NUMBER_HIDE = CARD_NUMBER.slice(0, -4).replace(
-  /123456789101/gi,
-  "************"
-);
-
-const CARD_NUMBER_HIDE_LAST_FOUR = CARD_NUMBER_HIDE.concat(
-  "",
-  CARD_NUMBER_LAST_FOUR
-);
-
-console.log(`Your card number: ${CARD_NUMBER_HIDE_LAST_FOUR}`);
-document.write(`Your card number: ${CARD_NUMBER_HIDE_LAST_FOUR}`);
+const CARD = prompt("Enter your car number: ", 1234567891011245);
+const CARD_LENGTH = CARD.length;
+const HIDE_PASSWORD = CARD.slice(0, CARD_LENGTH - 4);
+const RETURNER_PASSWORD = CARD.replace(`${HIDE_PASSWORD}`, "************");
+console.log(`Your card number is: ${RETURNER_PASSWORD}`);
